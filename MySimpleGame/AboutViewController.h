aac3265acef5f77b16257ded9b405dc11da7d0ca
Estique <estique@estiques-imac.local>
2016-10-03 19:18:57 +0600

    first commit: add necessary classes

---
+++ MySimpleGame/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  MySimpleGame
+//
+//  Created by Estique on 10/3/16.
+//  Copyright © 2016 Estique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
