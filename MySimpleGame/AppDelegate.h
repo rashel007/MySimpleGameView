1687bfc50ab31c704f324537e99e8af7994fe26c
Estique <estique@estiques-imac.local>
2016-10-03 19:10:21 +0600

    Initial Commit

---
+++ MySimpleGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySimpleGame
+//
+//  Created by Estique on 10/3/16.
+//  Copyright © 2016 Estique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySimpleGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MySimpleGame
+//
+//  Created by Estique on 10/3/16.
+//  Copyright © 2016 Estique. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
